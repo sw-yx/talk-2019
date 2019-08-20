0fc925cf412a16935bf8cef96d061c61d7070ee1
Josh Comeau <jcomeau@digitalocean.com>
2019-08-20 08:56:20 -0400

    WIP - fix prod builds
    Whenever I try and build for production, I get an error about `_ref2` not being defined.
    
    Some experimentation suggests that all styled media elements (styled.img and styled.video) don't work. I'm not sure why.
    
    I've started swapping them out, but it's tedious. Also, I'm pretty sure this is an issue specific to mdx or spectacle or something, and it feels bad to make my code worse for a build system issue.
    
    Given that I only need to run this presentation locally, I'm putting this effort on hold.

--- src/components/AnimationIntroScreen/AnimationIntroScreen.js
+++ src/components/AnimationIntroScreen/AnimationIntroScreen.js
@@ -6,7 +6,15 @@ import bokehBg from '../../../assets/cool-bokeh.mp4'
 const AnimationIntroScreen = ({ title }) => {
   return (
     <Wrapper>
-      <BackgroundVideo autoPlay loop src={bokehBg} />
+      <VideoWrapper>
+        <video
+          autoPlay
+          loop
+          src={bokehBg}
+          style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+        />
+      </VideoWrapper>
+
       <Foreground>
         <Subtitle>Animation Investigation</Subtitle>
         <Title>{title}</Title>
@@ -25,7 +33,7 @@ const Wrapper = styled.div`
 
 // normal|multiply|screen|overlay|darken|lighten|color-dodge|color-burn|difference|exclusion|hue|saturation|color|luminosity;
 
-const BackgroundVideo = styled.video`
+const VideoWrapper = styled.div`
   position: absolute;
   z-index: 0;
   top: 0;
